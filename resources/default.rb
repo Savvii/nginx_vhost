d0e08c10758130ba77ee0abd799d989fd7ded262
Bèr Kessels <ber@webschuur.com>
2013-10-28 12:36:53 +0100

    Change name to ssl_certificate and ssl_certificate_key,
    Since that is what nginx uses too. Lets not use arbitrary shortened
    words

--- resources/default.rb
+++ resources/default.rb
@@ -5,8 +5,10 @@ attribute :name, :name_attribute => true
 attribute :domains,  :kind_of => Array,  :default => ["_"]
 attribute :listen,   :kind_of => String, :default => "80" # alternatives: localhost:80 or unix:/striped/knitted.sock
 attribute :root_path,:kind_of => String, :required => true
-attribute :ssl_key,  :kind_of => String
-attribute :ssl_cert, :kind_of => String
+
+attribute :ssl_certificate_key, :kind_of => String
+attribute :ssl_certificate,     :kind_of => String
+
 attribute :log_dir,  :kind_of => String, :default => "/var/log/nginx/"
 attribute :nginx_dir,:kind_of => String, :default => "/etc/nginx/"
 
