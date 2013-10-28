38ed497108671bc4df2f40e0ea356d571e755d95
Bèr Kessels <ber@webschuur.com>
2013-10-28 15:27:39 +0100

    Use file.join insstead of hardcoded path-builders.
    A path is not a string

--- providers/default.rb
+++ providers/default.rb
@@ -8,10 +8,9 @@ def load_current_resource
 end
 
 action :enable do
-  [new_resource.log_dir,
-  new_resource.nginx_dir,
-  "#{new_resource.nginx_dir}/sites-enabled",
-  "#{new_resource.nginx_dir}/sites-available",
+  [new_resource.nginx_dir,
+  ::File.join(new_resource.nginx_dir, "sites-enabled"),
+  ::File.join(new_resource.nginx_dir, "sites-available"),
   ].each do |dir|
     directory dir  do
       recursive true
@@ -20,7 +19,7 @@ action :enable do
 
   name = new_resource.name
 
-  template "#{new_resource.nginx_dir}/sites-available/#{name}" do
+  template ::File.join(new_resource.nginx_dir, "sites-available", name) do
     source "vhost.conf.erb"
     mode "0644"
     owner "root"
@@ -28,16 +27,15 @@ action :enable do
     notifies     :reload, "service[nginx]"
   end
 
-  link "#{new_resource.nginx_dir}/sites-enabled/#{name}" do
-    to "#{new_resource.nginx_dir}/sites-available/#{name}"
+  link ::File.join(new_resource.nginx_dir, "sites-enabled", name) do
+    to ::File.join(new_resource.nginx_dir, "sites-available", name)
   end
 
   new_resource.updated_by_last_action(true)
-
 end
 
 action :disable do
-  link "#{new_resource.nginx_dir}/sites-available/#{name}" do
+  link ::File.join(new_resource.nginx_dir, "sites-available", name) do
     action :delete
     notifies     :reload, "service[nginx]"
   end
